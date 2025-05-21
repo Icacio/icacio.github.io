81f7d43bc993684a27a8b3deb3c3f139f6478513
Icacio <126359105+Icacio@users.noreply.github.com>
2025-05-21 00:41:22 -0600

    LocaleMode
    It just adds the interface to load the files from a custom server. But no logic implemented yet.

--- src/components/Caricatore.vue
+++ src/components/Caricatore.vue
@@ -13,8 +13,14 @@ const options = {
 	extensions: ['audio'],
 };
 const checkDropboxLoaded = null;
+const dropboxError = ref(false);
 
 onMounted(()=> {
+    let dropboxTimeout = setTimeout(() => {
+        if (typeof Dropbox === 'undefined') {
+			setLocal();
+        }
+    }, 5000); // 5 secondi di timeout
 	checkDropboxLoaded = setInterval(() => {
 		if (typeof Dropbox !== 'undefined') {
 			clearInterval(checkDropboxLoaded);
@@ -42,9 +48,27 @@ onBeforeUnmount(() => {
 });
 
 
+function setLocal() {
+	dropboxError.value = true;
+	clearInterval(checkDropboxLoaded);
+	let bu = document.getElementById("dropboxbutton")
+	if (bu) {
+		bu.addEventListener("click", async (e) => {
+			e.preventDefault();
+			let folder = document.getElementById("folder").value;
+			if (folder) {
+			}
+		})
+	}
+}
 
 </script>
 <template>
+	<div v-if="dropboxError">
+		<p>Error de conexión a Dropbox</p>
+		<p>Modo local activado</p>
+        <input type="text" id="folder"/>
+	</div>
 	<div>
 		<button id="dropboxbutton">{{ loading }}</button>
 	</div>
